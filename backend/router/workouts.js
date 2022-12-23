c209fdb1cad1a7f11068829582693a9f69332b98
MS_D_rei <46997561+MS-D-rei@users.noreply.github.com>
2022-12-23 18:32:43 +0900

    install mongoose and connect to MongoDB

--- backend/router/workouts.js
+++ backend/router/workouts.js
@@ -1,22 +1,27 @@
 const express = require("express");
 const router = express.Router();
 
+/* get all workouts */
 router.get("/", (req, res) => {
   res.json({ message: "Get all workouts" });
 });
 
+/* get the workout */
 router.get("/:workoutId", (req, res) => {
   res.json({ message: "Get the single workout" });
 });
 
+/* create new workout */
 router.post("/", (req, res) => {
   res.json({ message: "POST the new workout" });
 });
 
+/* delete the workout */
 router.delete("/:workoutId", (req, res) => {
   res.json({ message: "Delete the workout" });
 });
 
+/* update the workout */
 router.patch("/:workoutId", (req, res) => {
   res.json({ message: "Update the workout" });
 });
--- backend/server.js
+++ backend/server.js
@@ -1,28 +1,39 @@
-require('dotenv').config();
-const express = require('express')
+require("dotenv").config();
+const express = require("express");
 const workoutRoutes = require("./router/workouts");
+const mongoose = require("mongoose");
 
 /* create express app */
 const app = express();
 
-const hostname = 'localhost';
+const hostname = "localhost";
 const port = process.env.PORT;
 
 /* use middleware to parse json */
 app.use(express.json());
 
-app.listen(port, hostname, () => {
-  console.log(`Listening port: ${port}`);
-})
-
 app.use((req, res, next) => {
   console.log(req.path, req.method);
   next();
-})
+});
 
 app.get("/", (req, res) => {
-  res.json({ message: 'Hello, world json'});
-})
+  res.json({ message: "Hello, world json" });
+});
 
 /* workouts routes */
 app.use("/api/workouts", workoutRoutes);
+
+/* connect DB */
+mongoose.set("strictQuery", false);
+mongoose
+  .connect(process.env.DB_URL)
+  .then((result) => {
+    console.log("MongoDB connected");
+    app.listen(port, hostname, () => {
+      console.log(`Listening port: ${port}`);
+    });
+  })
+  .catch((err) => {
+    console.log(err);
+  });
