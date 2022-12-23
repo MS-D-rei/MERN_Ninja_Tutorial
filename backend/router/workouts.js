29b75c068ccdf71c411cf7c23a6c0a07d9d6a487
MS_D_rei <46997561+MS-D-rei@users.noreply.github.com>
2022-12-23 19:33:40 +0900

    add workout schema and post request function

--- backend/router/workouts.js
+++ backend/router/workouts.js
@@ -1,5 +1,6 @@
 const express = require("express");
 const router = express.Router();
+const Workout = require("../models/Workout");
 
 /* get all workouts */
 router.get("/", (req, res) => {
@@ -12,8 +13,18 @@ router.get("/:workoutId", (req, res) => {
 });
 
 /* create new workout */
-router.post("/", (req, res) => {
-  res.json({ message: "POST the new workout" });
+router.post("/", async (req, res) => {
+  const { title, reps, load } = req.body;
+  try {
+    const workout = await Workout.create({ title, reps, load });
+    res.status(200).json(workout);
+  } catch (err) {
+    if (err instanceof Error) {
+      res.status(400).json({ error: err.message });
+    } else {
+      res.status(400).json({ error: err });
+    }
+  }
 });
 
 /* delete the workout */
