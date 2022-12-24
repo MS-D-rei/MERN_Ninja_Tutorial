90502116dc7d99648fe32d180541d8008d511d47
MS_D_rei <46997561+MS-D-rei@users.noreply.github.com>
2022-12-24 15:13:11 +0900

    add fetchWorktouts to get workout data from backend

--- frontend/src/pages/Home.tsx
+++ frontend/src/pages/Home.tsx
@@ -1,14 +1,27 @@
-import { useEffect } from "react";
+import { useEffect, useState } from 'react';
+import { IWorkout } from '@/types/workout-type';
 
 export default function Home() {
+  const [workouts, setWorkouts] = useState<IWorkout[]>();
+
   useEffect(() => {
     const fetchWorkouts = async () => {
-      const response = fetch('http://localhost:4000/api/workouts')
-    }
-  }, [])
+      const response = await fetch('http://localhost:4000/api/workouts');
+      const data = await response.json();
+      if (response.ok) {
+        setWorkouts(data);
+      }
+    };
+    fetchWorkouts();
+  }, []);
+
   return (
-    <div className="Home">
-      <h2>Home</h2>
+    <div className="home">
+      <div className="workout">
+        <h2>Home</h2>
+        {workouts &&
+          workouts.map((workout) => <p key={workout._id}>{workout.title}</p>)}
+      </div>
     </div>
   );
 }
