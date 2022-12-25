d4b5e0c0f8eced53a984f58ff6e385be971ea7f1
MS_D_rei <46997561+MS-D-rei@users.noreply.github.com>
2022-12-25 12:15:42 +0900

    modify CORS setting

--- backend/server.js
+++ backend/server.js
@@ -11,7 +11,7 @@ const hostname = "localhost";
 const port = process.env.PORT;
 
 /* CORS setting */
-const accessControlAllowOrigins = ["http://localhost:5173"];
+const accessControlAllowOrigins = ["http://localhost:5173", "http://127.0.0.1:5173"];
 const accessControlAllowMethods = [
   "GET",
   "HEAD",
