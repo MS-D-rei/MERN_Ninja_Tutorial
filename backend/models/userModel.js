6b4d8b6732dd5524c1db8ddcd4e7b5f0392df51b
MS_D_rei <46997561+MS-D-rei@users.noreply.github.com>
2022-12-27 12:59:59 +0900

    add user signup validation to userModel

--- backend/models/userModel.js
+++ backend/models/userModel.js
@@ -1,6 +1,7 @@
 const bcrypt = require("bcrypt");
 const mongoose = require("mongoose");
 const { Schema } = mongoose;
+const validator = require("validator");
 
 const userSchema = new Schema({
   name: {
@@ -23,11 +24,38 @@ const saltRound = 10;
 
 /* sign up static method */
 userSchema.statics.signup = async function (name, email, password) {
+  // validation
+  if (!name || !email || !password) {
+    throw new Error("All fields must be filled");
+  }
+
+  let errorMessages = []
+
+  if (!validator.isEmail(email)) {
+    errorMessages.push("Email is not valid");
+  }
+  if (
+    !validator.isStrongPassword(password, {
+      minLength: 8,
+      minLowercase: 1,
+      minUppercase: 0,
+      minNumbers: 0,
+      minSymbols: 0,
+    })
+  ) {
+    errorMessages.push("Password is not valid");
+  }
+
+  if (errorMessages.length > 0) {
+    throw new Error(errorMessages);
+  }
+
   const isExists = await this.findOne({ email });
   if (isExists) {
     throw Error("The email already exists");
   }
 
+  // hash password
   const salt = await bcrypt.genSalt(saltRound);
   const hash = await bcrypt.hash(password, salt);
   const user = await this.create({ name, email, password: hash });
