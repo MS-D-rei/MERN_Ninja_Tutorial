d64f9416beb29818353c38a949a5996be8776f70
MS_D_rei <46997561+MS-D-rei@users.noreply.github.com>
2022-12-27 16:51:58 +0900

    add create token function to userController

--- backend/controllers/userController.js
+++ backend/controllers/userController.js
@@ -1,4 +1,9 @@
 const User = require("../models/userModel");
+const jwt = require("jsonwebtoken");
+
+const createToken = (_id) => {
+  return jwt.sign({ _id }, process.env.JWT_SECRET, { expiresIn: "3d" });
+};
 
 const loginUser = async (req, res) => {
   res.json({ message: "Login user" });
@@ -9,7 +14,8 @@ const signupUser = async (req, res) => {
 
   try {
     const user = await User.signup(name, email, password);
-    res.status(200).json({ email, user });
+    const token = createToken(user._id);
+    res.status(200).json({ email, token });
   } catch (err) {
     res.status(400).json({ error: err.message });
   }
