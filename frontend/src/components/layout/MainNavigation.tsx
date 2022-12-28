0c7d457825387b77a8e263371bc7018f931da6a7
MS_D_rei <46997561+MS-D-rei@users.noreply.github.com>
2022-12-28 16:28:41 +0900

    modify css of Layout and MainNavigation

--- frontend/src/components/layout/MainNavigation.tsx
+++ frontend/src/components/layout/MainNavigation.tsx
@@ -1,13 +1,30 @@
-import { Link } from "react-router-dom";
-import styles from '@/styles/components/layout/MainNavigation.module.css'
+import { Link } from 'react-router-dom';
+import styles from '@/styles/components/layout/MainNavigation.module.css';
+import { useAppSelector } from '@/hooks/workoutsHook';
 
 export default function MainNavigation() {
+  const userState = useAppSelector((state) => state.user);
+  const { isLoggedIn } = userState;
   return (
-    <header>
+    <header className={styles.header}>
       <div className={styles.container}>
-        <Link to="/">
-          <h1>Workout Buddy</h1>
-        </Link>
+        <div className={styles.logo}>
+          <Link to="/">Workout Todo</Link>
+        </div>
+        <nav className={styles.nav}>
+          <ul>
+            {!isLoggedIn && (
+              <>
+                <li>
+                  <Link to="/login">Login</Link>
+                </li>
+                <li>
+                  <Link to="/signup">Signup</Link>
+                </li>
+              </>
+            )}
+          </ul>
+        </nav>
       </div>
     </header>
   );
