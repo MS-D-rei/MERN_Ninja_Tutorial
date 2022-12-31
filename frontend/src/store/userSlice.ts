25625fdfc288e194c73180646f74326319815761
MS_D_rei <46997561+MS-D-rei@users.noreply.github.com>
2022-12-31 17:39:03 +0900

    add login async thunk function to userSlice

--- frontend/src/store/userSlice.ts
+++ frontend/src/store/userSlice.ts
@@ -43,7 +43,7 @@ export const signup = createAsyncThunk<
   { name: string; email: string; password: string },
   // types for thunkAPI
   { rejectValue: INotOkResponse; serializedErrorType: INotOkResponse }
->('/user/signup', async ({ name, email, password }, thunkApi) => {
+>('/user/signup', async ({ name, email, password }, thunkAPI) => {
   const signupRequestBody = {
     name,
     email,
@@ -61,7 +61,7 @@ export const signup = createAsyncThunk<
     const data: IResponsePayload | INotOkResponse = await response.json();
 
     if (!response.ok) {
-      return thunkApi.rejectWithValue(data as INotOkResponse);
+      return thunkAPI.rejectWithValue(data as INotOkResponse);
     }
 
     localStorage.setItem('user', JSON.stringify(data));
@@ -69,9 +69,51 @@ export const signup = createAsyncThunk<
     return data as IResponsePayload;
   } catch (err) {
     if (err instanceof Error) {
-      return thunkApi.rejectWithValue({ message: err.message });
+      return thunkAPI.rejectWithValue({ message: err.message });
     } else {
-      return thunkApi.rejectWithValue({ message: `Unexpected Error: ${err}` });
+      return thunkAPI.rejectWithValue({ message: `Unexpected Error: ${err}` });
+    }
+  }
+});
+
+export const login = createAsyncThunk<
+  // return type
+  IResponsePayload | INotOkResponse,
+  // args type
+  { email: string; password: string },
+  // thunkAPI type option
+  { rejectValue: INotOkResponse; serializedErrorType: INotOkResponse }
+>('/user/login', async ({ email, password }, thunkAPI) => {
+  const loginRequestBoby = {
+    email,
+    password,
+  };
+
+  try {
+    const response = await fetch('http://localhost:4000/api/user/login', {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json',
+      },
+      body: JSON.stringify(loginRequestBoby),
+    });
+
+    const data: IResponsePayload | INotOkResponse = await response.json();
+
+    // in case of 400
+    if (!response.ok) {
+      return thunkAPI.rejectWithValue(data as INotOkResponse);
+    }
+
+    // store data in localstorage
+    localStorage.setItem('user', JSON.stringify(data));
+
+    return data as IResponsePayload;
+  } catch (err) {
+    if (err instanceof Error) {
+      return thunkAPI.rejectWithValue({ message: err.message });
+    } else {
+      return thunkAPI.rejectWithValue({ message: `Unexpected Error: ${err}` });
     }
   }
 });
@@ -94,9 +136,10 @@ export const userSlice = createSlice({
     },
     resetError: (state) => {
       state.error = null;
-    } 
+    },
   },
   extraReducers: (builder) => {
+    // signup async thunk
     builder.addCase(signup.pending, (state, action) => {
       state.status = 'loading';
     }),
@@ -109,6 +152,26 @@ export const userSlice = createSlice({
         state.error = null;
       }),
       builder.addCase(signup.rejected, (state, action) => {
+        state.status = 'failed';
+        if (action.payload) {
+          state.error = action.payload;
+        } else {
+          state.error = action.error;
+        }
+      });
+    // login async thunk
+    builder.addCase(login.pending, (state) => {
+      state.status = 'loading';
+    }),
+      builder.addCase(login.fulfilled, (state, action) => {
+        state.status = 'succeeded';
+        state.name = action.payload.name;
+        state.email = action.payload.email;
+        state.idToken = action.payload.idToken;
+        state.isLoggedIn = true;
+        state.error = null;
+      }),
+      builder.addCase(login.rejected, (state, action) => {
         state.status = 'failed';
         if (action.payload) {
           state.error = action.payload;
